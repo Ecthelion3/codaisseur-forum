5628315012d71b06e9d0bc12fa480fb0c77b6df3
Wouter de Vos <wouter@codaisseur.com>
2016-08-31 10:38:01 +0200

    Load default avatar over https

--- config/initializers/gravatar_image_tag.rb
+++ config/initializers/gravatar_image_tag.rb
@@ -1,5 +1,5 @@
 GravatarImageTag.configure do |config|
-  config.default_image           = "http://cd.sseu.re/Codaisseur-square-logo-512.png"
+  config.default_image           = "https://s3-eu-west-1.amazonaws.com/cd.sseu.re/Codaisseur-square-logo-512.png"
   config.include_size_attributes = false
   config.size                    = 512
   config.secure                  = true
